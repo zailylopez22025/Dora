153e8284b68995501a00eea0b227ea635810df10
denivyruck <denivyruck1@gmail.com>
2025-07-15 22:57:24 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Dora",
-    version="0.0.4",
+    version="0.0.5",
     author="Nathan Epstein",
     author_email="ne2210@columbia.edu",
     description="Exploratory data analysis toolkit for Python",
